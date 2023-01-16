272cfbd1922c3f0149fc85a524f80c16f920995c
Aksel Thomas <20477698+ast96@users.noreply.github.com>
2023-01-16 21:12:14 +0000

    Verify connection to board

--- raspberry_pi/script.py
+++ raspberry_pi/script.py
@@ -4,3 +4,7 @@ import adafruit_scd30
 
 def create_file(file):
     Path.touch(file)
+
+def connect_to_board():
+    sensor_board = adafruit_scd30.SCD30(board.I2C())
+    return sensor_board.data_available
--- raspberry_pi/test_script.py
+++ raspberry_pi/test_script.py
@@ -5,3 +5,6 @@ def test_create_file(tmp_path):
     file = tmp_path / 'test-output.txt'
     script.create_file(file)
     assert Path(file).exists()
+
+def test_connect_to_board():
+    assert script.connect_to_board() == 1
