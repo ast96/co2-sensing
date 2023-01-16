5c5c101175d048e740fab9456074f96d9bf42ec5
Aksel Thomas <20477698+ast96@users.noreply.github.com>
2023-01-16 21:29:29 +0000

    Instantiate board functionally

--- raspberry_pi/script.py
+++ raspberry_pi/script.py
@@ -6,5 +6,7 @@ def create_file(file):
     Path.touch(file)
 
 def connect_to_board():
-    sensor_board = adafruit_scd30.SCD30(board.I2C())
+    return adafruit_scd30.SCD30(board.I2C())
+
+def check_connection_to_board(sensor_board):
     return sensor_board.data_available
--- raspberry_pi/test_script.py
+++ raspberry_pi/test_script.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import board
+import adafruit_scd30
 import script
 
 def test_create_file(tmp_path):
@@ -6,5 +8,9 @@ def test_create_file(tmp_path):
     script.create_file(file)
     assert Path(file).exists()
 
+def test_check_connection_to_board():
+    sensor_board = adafruit_scd30.SCD30(board.I2C())
+    assert script.check_connection_to_board(sensor_board) == 1
+
 def test_connect_to_board():
-    assert script.connect_to_board() == 1
+    assert isinstance(script.connect_to_board(), adafruit_scd30.SCD30)
