d9af2adf932dfa0a2a1d083878fdd26d9781c7eb
Aksel Thomas <20477698+ast96@users.noreply.github.com>
2023-01-21 20:53:28 +0000

    Add data collection loop

--- raspberry_pi/script.py
+++ raspberry_pi/script.py
@@ -37,13 +37,22 @@ def write_line_to_file(line, file):
     file.write(line + '\n')
 
 def main():
+    MAX_ITERATIONS = 360    # 360 reads = 180 min / 0.5 min/read (3 hrs)
     sensor_board = connect_to_board()
+    collect_data_row(sensor_board)      # warm up the sensor
     # note that the file got created by `root`, not my user
     path = Path(get_output_directory() + '/' + generate_unique_filename())
     create_file(path)
     with open(path, 'a') as f:
         write_line_to_file(make_header_row(), f)
-        write_line_to_file(collect_data_row(sensor_board), f)
+        i = 0
+        while i < MAX_ITERATIONS:
+            write_line_to_file(collect_data_row(sensor_board), f)
+            time.sleep(30)
+            i++
+
+    # then, test it by running it for 2 minutes, instead
+    # (optionally) have it shut down the raspberry pi at the end
 
 if __name__ == '__main__':
     main()
